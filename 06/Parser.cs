35e05de7cfe80202123c75564650204c2719a49f
NeMoSemi <115030464+NeMoSemi@users.noreply.github.com>
2025-12-10 21:02:48 +0500

    Add files via upload

---
+++ 06/Parser.cs
@@ -0,0 +1,19 @@
+namespace Assembler
+{
+    public class Parser
+    {
+        public string[] RemoveWhitespacesAndComments(string[] asmLines)
+        {
+            var withoutComments = new List<string>();
+            foreach (var line in asmLines)
+            {
+                var trimmedLine = line.Split("//")[0].Trim();
+                if (!string.IsNullOrEmpty(trimmedLine))
+                {
+                    withoutComments.Add(trimmedLine.Replace(" ", ""));
+                }
+            }
+            return withoutComments.ToArray();
+        }
+    }
+}
